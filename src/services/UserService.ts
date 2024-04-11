4150d53b23fd19c3836e708501f557a491efaa65
hsrk12 <hs12rk@gmail.com>
2024-04-10 17:34:06 -0700

    added insert logic

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -16,13 +16,22 @@ public insert_user(user_data: any): void{
         }
         console.log('Connected to the fin database.');
         console.log('inserting User record');
+        
+        //TODO: check if user already exists
+        db.run('INSERT INTO Users(userID, user_name, user_email, user_phone) VALUES (?,?,?,?)',[user_data.userID, user_data.user_name, user_data.user_email, user_data.user_phone], function(err){
+            if (err) {
+                return console.log(err.message);
+              }
+              // get the last insert id
+              console.log(`A row has been inserted for ` + user_data.user_name);
+        });
     
     });
     db.close();
 }
 
 public updateUser(userData: any): void{
-    
+
 }
 
 
